94a6718e2ec5dd0f81e28f69fb9e2da336e1d5ad
caiovellani <caiovellani@outlook.com>
2024-07-09 22:26:36 -0300

    feat: create activities routes

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmParticipants } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
+import { createActivity } from "./routes/create-activity";
 import { createTrip } from "./routes/create-trip";
+import { getActivity } from "./routes/get-activities";
 
 
 const app = fastify();
@@ -18,6 +20,8 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivity)
 
 app.listen({ port: 3333}).then(() => {
     console.log('Server Running!')
