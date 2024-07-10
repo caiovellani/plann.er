49a1e12150f7f3ba12a1d6ed75a84b9003add2f0
caiovellani <caiovellani@outlook.com>
2024-07-10 20:21:24 -0300

    feat: create update-trip route

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { confirmParticipants } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
 import { createActivity } from "./routes/create-activity";
+import { createInvite } from "./routes/create-invite";
 import { createLink } from "./routes/create-link";
 import { createTrip } from "./routes/create-trip";
 import { getActivity } from "./routes/get-activities";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
+import { updateTrip } from "./routes/update-trip";
 
 
 const app = fastify();
@@ -28,6 +30,8 @@ app.register(getActivity)
 app.register(createLink)
 app.register(getLinks)
 app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
 
 app.listen({ port: 3333}).then(() => {
     console.log('Server Running!')
